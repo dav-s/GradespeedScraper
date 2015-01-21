dd0d5f5f809d3723c4a818fb94ee66ba8ba2a7db
Davis Robertson <davis@daviskr.com>
2015-01-21 14:41:56 -0600

    Adding Specific Grades GUI
    Expanding on the `Simplifying Code` commit, this commit further
    simplifies things and adds the specific grades overview. The GUI needs
    to be worked on for styling an such.

--- runner.py
+++ runner.py
@@ -1,9 +1,12 @@
 from gradespeedscraper.wrapper import Wrapper, Link
 from gradespeedscraper.tkgui_utils import center_gui
 from gradespeedscraper.stringcode import encode_string, decode_string
-from Tkinter import Tk, Label, StringVar, Entry, BooleanVar, Frame, Checkbutton, Button
-import json
+from Tkinter import Tk, StringVar, BooleanVar, Frame, Label, LabelFrame, Button, Checkbutton, Entry
+#from ttk import Label, Frame, Checkbutton, Button, LabelFrame, Entry
+from config import main_url
 import os
+
+wrap = Wrapper(main_url)
 
 
 def login_gui(username, password):
@@ -14,11 +17,11 @@ def login_gui(username, password):
             logon_file = open("dep.dat", "w")
             logon_file.write(encode_string(temp_username+" "+temp_password))
             logon_file.close()
-        else:
-            if os.path.isfile("dep.dat"):
-                os.remove("dep.dat")
+        elif os.path.isfile("dep.dat"):
+            os.remove("dep.dat")
         login_tk.destroy()
-        wrapper_gui(temp_username, temp_password)
+        wrap.login(temp_username, temp_password)
+        overview_gui()
         return
 
     def on_checkbox_flip():
@@ -59,20 +62,53 @@ def login_gui(username, password):
     login_tk.mainloop()
 
 
-def wrapper_gui(username, password):
-    wrap = Wrapper(config["specific-url"])
-    wrap.login(username, password)
-    res = wrap.get_student_grades_overview()
+def specific_frame(gui, link):
+    res = wrap.get_class_grades(link)
+    main_frame = LabelFrame(gui, text=res["class_name"] + " - " + res["current_average"])
+    for sec in res["sections"]:
+        frame_name = sec["name"]
+        if sec["average"]:
+            frame_name += " - " + sec["average"]
+        temp_frame = LabelFrame(main_frame, text=frame_name)
+        for i, h in enumerate(sec["grades"]["headers"]):
+            Label(temp_frame, text=h).grid(row=1, column=i)
+        for i, r in enumerate(sec["grades"]["rows"]):
+            for j, c in enumerate(r):
+                Label(temp_frame, text=c).grid(row=i+2, column=j)
+        temp_frame.pack()
+    main_frame.pack()
+
+
+def specific_gui(link):
     gui = Tk()
-    gui.title(res["student_name"])
+    overview_frame(gui)
+    specific_frame(gui, link)
+    center_gui(gui)
+    gui.mainloop()
+
+
+def overview_frame(gui):
+    def click_link(link):
+        gui.destroy()
+        specific_gui(link)
+    res = wrap.get_student_grades_overview()
+    frame = LabelFrame(gui, text=res["student_name"])
     for i, t in enumerate(res["grades"]["headers"]):
-        Label(gui, text=str(t)).grid(row=0, column=i)
+        Label(frame, text=str(t)).grid(row=0, column=i)
     for i, r in enumerate(res["grades"]["rows"]):
         for j, c in enumerate(r):
             if isinstance(c, Link):
-                Button(gui, text=str(c)).grid(row=i+1, column=j)
+                Button(frame, command=lambda l=c: click_link(l), text=str(c))\
+                    .grid(row=i+1, column=j)
             elif c:
-                Label(gui, text=str(c)).grid(row=i+1, column=j)
+                Label(frame, text=str(c)).grid(row=i+1, column=j)
+    frame.pack()
+    gui.title(res["student_name"])
+
+
+def overview_gui():
+    gui = Tk()
+    overview_frame(gui)
     center_gui(gui)
     gui.mainloop()
 
@@ -87,16 +123,6 @@ def get_file_tuple():
 
 
 def main():
-    global config
-
-    if os.path.isfile("config.json"):
-        conf_file = open("config.json")
-        config = json.loads(conf_file.read())
-        conf_file.close()
-    else:
-        print 'You need to have the "config.json".'
-        return
-
     login_gui(*(get_file_tuple()))
 
 if __name__ == "__main__":
